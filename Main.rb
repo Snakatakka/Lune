227ff738f66c79f8f8708da8bde451b2814d81e4
Snakatakka <etwork563@gmail.com>
2026-05-16 11:02:58 -0400

    refactor Main.rb

--- Main.rb
+++ Main.rb
@@ -24,7 +24,15 @@ autoplay = false
 
 $current_song.play
 
-
+def change_song(new_song_id)
+    $current_song.stop
+    $current_song_title.remove
+    $song_id = new_song_id
+    $current_song = Music.new("tracks\\#{$songs[$song_id]}")
+    $current_song_title = Text.new("#{$songs[$song_id].slice(0..($songs[$song_id].index('.') - 1))}", z: 10)
+    $current_song_title.add
+    $current_song.play
+end
 
 # user interface
 # -----------------------------------------------------------------------------------------------------------------
@@ -55,7 +63,7 @@ main_screen = Element.new(
     "rectangle", # type
     (main_outer.get_attribute("x_pos")), # x_pos
     (main_outer.get_attribute("y_pos")), # y_pos
-    1, # z_index
+    2, # z_index
     (main_inner.get_attribute("height") - 10), # height
     (main_inner.get_attribute("width") - 10), # width
     0, # size
@@ -67,66 +75,49 @@ main_screen = Element.new(
 
 paused = false
 
+def shuffle()
+    shuffled_song = rand(0..($songs.length - 1))
+    
+    unless shuffled_song == $songs[song_id] # prevents shuffle from choosing the song that's currently playing
+        change_song(shuffled_song)
+    else
+        change_song(shuffled_song + 1)
+end
+
 def reload()
-    $current_song.stop
-    $current_song_title.remove
     $songs = get_songs("tracks")
-    $current_song = Music.new("tracks\\#{$songs[0]}")
-    $songs = get_songs("tracks")
-    $current_song_title.add
-    $current_song.play
+    change_song(0)
 end
 
 def cycle_forward()
-    $current_song.stop
-    $current_song_title.remove
     unless $song_id + 1 >= $songs.length
-        $song_id += 1
-        $current_song = Music.new("tracks\\#{$songs[$song_id]}")
-        $current_song_title = Text.new("#{$songs[$song_id].slice(0..($songs[$song_id].index('.') - 1))}")
+        change_song($song_id + 1)
     else
-        $song_id = 0
-        $current_song = Music.new("tracks\\#{$songs[$song_id]}")
-        $current_song_title = Text.new("#{$songs[$song_id].slice(0..($songs[$song_id].index('.') - 1))}")
+        change_song(0)
     end
-    $current_song_title.add
-    $current_song.play
 end
 
 def cycle_backward()
-    $current_song.stop
-    $current_song_title.remove
     unless $song_id - 1 < 0
-        $song_id -= 1
-        $current_song = Music.new("tracks\\#{$songs[$song_id]}")
-        $current_song_title = Text.new("#{$songs[$song_id].slice(0..($songs[$song_id].index('.') - 1))}")
+        change_song($song_id - 1)
     else
-        $song_id = $songs.length - 1
-        $current_song = Music.new("tracks\\#{$songs[$song_id]}")
-        $current_song_title = Text.new("#{$songs[$song_id].slice(0..($songs[$song_id].index('.') - 1))}")
+        change_song($songs.length - 1)
     end
-    $current_song_title.add
-    $current_song.play
 end
 
 
-on :key_down do |input| # get input from user
+on :key_down do |input| # keyboard input
     case input.key
-    when "p" # keyboard controls for pausing
+    when "p" # pause song
         paused = !paused
-    
     when "s" # shuffle song
-        # TODO: add shuffle function
-
+        shuffle()
     when "r" # reload app
         reload()
-
     when "right" # cycle song forward
         cycle_forward()
-
     when "left" # cycle song backwards
         cycle_backward()
-
     when "escape" # quit
         close
     end
